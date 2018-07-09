b94fc41cca4599a02a91d99a25952fc7f48f5347
lschipelliti <37881922+lschipelliti@users.noreply.github.com>
2018-07-08 23:57:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,100 +1,44 @@
-const config = require('./config.json');
 const Discord = require('discord.js');
-const util = require('util');
-const bot = new Discord.Client({
-    disableEveryone: true,
-    disabledEvents: ['TYPING_START']
+const client = new Discord.Client();
+const token = "NDY1NzIxNDcyMTkxNTYxNzI4.DiRokw.Nnh2owu3_cMn5AM6_a7avK-DvtY";//put discord bot token here
+const SERVER_ID = "450098535946256414";
+const CHANNEL_ID = "450118376204009482";
+const PREM_ID = "450118281085452311";
+const UPGRADE_MESSAGE = "Congratulations, you have been upgraded to @Premium in Premium Signals!";
+const DOWNGRADE_MESSAGE = "Unfortunately, your Premium Signals membership has expired. If you would like to renew your membership for just $1/day, please visit http://www.PremiumSignals.info";
+const UPGRADE_PREFIX = "_upgrade";
+const DOWNGRADE_PREFIX = "_downgrade";
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+
 });
 
-bot.on("ready", () => {
-    bot.user.setGame('Awesome Fun Game'); //you can set a default game
-    console.log(`Bot is online!\n${bot.users.size} users, in ${bot.guilds.size} servers connected.`);
+client.on('message', message => {
+	if(message.channel.id != CHANNEL_ID || message.author.id === client.user.id){
+		return;
+	}
+	let content = message.content;
+	
+	console.log(content);
+	if(content.startsWith(UPGRADE_PREFIX)){
+		let userid = content.substr(UPGRADE_PREFIX.length + 3, 18);
+		
+		console.log(userid);
+		let user = client.guilds.get(SERVER_ID).members.get(userid);
+		user.addRole(PREM_ID);
+		user.send(UPGRADE_MESSAGE)
+			.catch(console.error);
+	} else if(content.startsWith(DOWNGRADE_PREFIX)){
+		let userid = content.substr(DOWNGRADE_PREFIX.length + 3, 18);
+		
+		console.log(userid);
+		let user = client.guilds.get(SERVER_ID).members.get(userid);
+		user.removeRole(PREM_ID);
+		user.send(UPGRADE_MESSAGE)
+			.catch(console.error);
+	}
 });
 
-bot.on("guildCreate", guild => {
-    console.log(`I've joined the guild ${guild.name} (${guild.id}), owned by ${guild.owner.user.username} (${guild.owner.user.id}).`);
-});
 
-bot.on("message", async message => { 
-
-    if(message.author.bot || message.system) return; // Ignore bots
-    
-    if(message.channel.type === 'dm') { // Direct Message
-        return; //Optionally handle direct messages
-    } 
-
-    console.log(message.content); // Log chat to console for debugging/testing
-    
-    if (message.content.indexOf(config.prefix) === 0) { // Message starts with your prefix
-        
-        let msg = message.content.slice(config.prefix.length); // slice of the prefix on the message
-
-        let args = msg.split(" "); // break the message into part by spaces
-
-        let cmd = args[0].toLowerCase(); // set the first word as the command in lowercase just in case
-
-        args.shift(); // delete the first word from the args
-
-        
-        if (cmd === 'hi' || cmd === 'hello') { // the first command [I don't like ping > pong]
-            message.channel.send(`Hi there ${message.author.toString()}`);
-            return; 
-        }
-
-        else if (cmd === 'ping') { // ping > pong just in case..
-            return message.channel.send('pong');
-        }
-
-        // Make sure this command always checks for you. YOU NEVER WANT ANYONE ELSE TO USE THIS COMMAND
-        else if (cmd === "eval" && message.author.id === config.owner){ // < checks the message author's id to yours in config.json.
-            const code = args.join(" ");
-            return evalCmd(message, code);
-        }
-
-        else { // if the command doesn't match anything you can say something or just ignore it
-            message.channel.send(`I don't know what command that is.`);
-            return;
-        }
-        
-    } else if (message.content.indexOf("<@"+bot.user.id) === 0 || message.content.indexOf("<@!"+bot.user.id) === 0) { // Catch @Mentions
-
-        return message.channel.send(`Use \`${config.prefix}\` to interact with me.`); //help people learn your prefix
-    }
-    return;
-});
-
-function evalCmd(message, code) {
-    if(message.author.id !== config.owner) return;
-    try {
-        let evaled = eval(code);
-        if (typeof evaled !== "string")
-            evaled = util.inspect(evaled);
-            message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-}
-function clean(text) {
-    if (typeof(text) !== 'string') {
-        text = util.inspect(text, { depth: 0 });
-    }
-    text = text
-        .replace(/`/g, '`' + String.fromCharCode(8203))
-        .replace(/@/g, '@' + String.fromCharCode(8203))
-        .replace(config.token, 'mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0') //Don't let it post your token
-    return text;
-}
-
-// Catch Errors before they crash the app.
-process.on('uncaughtException', (err) => {
-    const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
-    console.error('Uncaught Exception: ', errorMsg);
-    // process.exit(1); //Eh, should be fine, but maybe handle this?
-});
-
-process.on('unhandledRejection', err => {
-    console.error('Uncaught Promise Error: ', err);
-    // process.exit(1); //Eh, should be fine, but maybe handle this?
-});
-
-bot.login(config.token);
+client.login(token);
